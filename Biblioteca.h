947784d1b997aeff515069466ee7ec945abbecde
Marcuzss <146141363+Marcuzss@users.noreply.github.com>
2023-10-16 19:42:48 -0300

    Update Biblioteca.h
    Definimos as informações que serão armazenadas dos clientes, que serão o nome, senha, cpf, saldo e o tipo de conta.

--- Biblioteca.h
+++ Biblioteca.h
@@ -9,12 +9,3 @@ typedef struct {
     char senha[20];
 } Cliente;
 
-void novoCliente(Cliente *clientes, int *numClientes);
-void apagaCliente(Cliente *clientes, int *numClientes);
-void listarClientes(Cliente *clientes, int numClientes);
-void debito(Cliente *clientes, int numClientes);
-void deposito(Cliente *clientes, int numClientes);
-void extrato(Cliente *clientes, int numClientes);
-void transferencia(Cliente *clientes, int numClientes);
-
-#endif
